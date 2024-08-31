d6a4a0764d74d1d108827543584b10af6488e6e8
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-08-31 15:30:23 -0400

    done

---
+++ src/helpers/StateProvider.tsx
@@ -0,0 +1,78 @@
+import {
+  useState,
+  useEffect,
+  useRef,
+  FC,
+  ReactNode,
+  SetStateAction,
+  Dispatch,
+} from "react";
+import { createContext, useContext } from "react";
+import { socket } from "../routes/socket";
+
+export type GameStateType = {
+  mode: "start" | "topic" | "ask" | "results" | "end";
+  ask_state: { prompt: string; answers: Map<string, string> } | undefined;
+  topic_state: { topic: string } | undefined;
+  elapsed_rounds: number;
+  count_time: number | undefined;
+  users: Array<{ name: string; score: number; id: string }>;
+  last_winner: string | undefined;
+};
+
+export const AccessContext = createContext<{
+  gamestate: GameStateType;
+}>({
+  gamestate: {
+    mode: "start",
+    ask_state: undefined,
+    topic_state: undefined,
+    users: [],
+    count_time: undefined,
+    elapsed_rounds: 0,
+    last_winner: undefined,
+  },
+});
+
+type Props = {
+  children: ReactNode;
+};
+
+export const StateProvider: FC<Props> = ({ children }) => {
+  const [gamestate, setGameState] = useState<GameStateType>({
+    mode: "start",
+    ask_state: undefined,
+    topic_state: undefined,
+    users: [],
+    count_time: undefined,
+    elapsed_rounds: 0,
+    last_winner: undefined,
+  });
+
+  const clientId = "2695e07f91b64a2bbc0e4551654a330a";
+  // const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+
+  // function seekSong()
+  useEffect(() => {
+    socket.on("gamestate", (state) => {
+      setGameState(state);
+    });
+
+    return () => {
+      // socket.removeListener("newuser");
+      socket.removeListener("gamestate");
+    };
+  }, []);
+
+  return (
+    <>
+      <AccessContext.Provider
+        value={{
+          gamestate: gamestate,
+        }}
+      >
+        {children}
+      </AccessContext.Provider>
+    </>
+  );
+};
