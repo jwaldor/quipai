6e6fa939978093ef8a4ffe2fd88f40d5e44210fa
Marcus Kiisa <marcus.kiisa@gmail.com>
2024-08-31 17:19:31 -0400

    disabled state

--- src/pages/GameStart.tsx
+++ src/pages/GameStart.tsx
@@ -94,18 +94,18 @@ const GameStart: React.FC = () => {
       </form> : <div className="text-center mt-4">
         <p className="text-white text-lg font-semibold">You have joined the game. Waiting for the host to start...</p>
       </div>}
-      <div className="flex justify-center mt-2 sm:mt-16 md:scale-150">
+      {disabled && <div className="flex justify-center mt-2 sm:mt-16 md:scale-150">
         <button
           type="submit"
           onClick={() => {
             console.log("begin game");
             socket.emit("begingame");
           }}
-          className="bg-green-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          className="bg-green-500 text-white px-4 py-2 mb-3 rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
         >
           Start game
         </button>
-      </div>
+      </div>}
     </div>
   );
 };
